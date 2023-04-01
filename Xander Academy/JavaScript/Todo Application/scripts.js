bcfe177aacffb91ac79df051723c0e2bf0baa8d6
ctorrington1 <ctorrington1@gmail.com>
2023-04-01 19:43:01 +0100

    added tasks can be displayed

--- Xander Academy/JavaScript/Todo Application/scripts.js
+++ Xander Academy/JavaScript/Todo Application/scripts.js
@@ -3,9 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // Document elements.
     let new_task_input = document.getElementById('new_task_input');
     let new_task_form = document.getElementById('new_task_form');
+    let task_list = document.getElementById('task_list');
 
     // Variables.
     let taskList = [];
+    
 
 
     // Event listeners.
@@ -21,11 +23,30 @@ document.addEventListener("DOMContentLoaded", () => {
         // Continue if task is entered & enter key is pressed.
         if (e.key === "Enter" && new_task_input.value != "") {
             addNewTask(new_task_input.value);
+            createNewTaskListItem(new_task_input.value);
             new_task_input.value = "";
         };
 
         
     });
+
+
+    // Create new list item.
+    function createNewTaskListItem(task) {
+
+        // Create the list item element.
+        let new_task_list_item = document.createElement('li');
+
+        // Create the actual list item value.     See more, https://www.w3schools.com/jsref/met_node_appendchild.asp
+        let new_task_list_item_text_node = document.createTextNode(task);
+
+        // Append the text node to the list item element,
+        // i.e. give the list item a value.
+        new_task_list_item.appendChild(new_task_list_item_text_node);
+
+        // Add the new list item, now with a value, to the unordered list.
+        task_list.appendChild(new_task_list_item);
+    };
 
 
     // Add the entered task to the list of tasks.
